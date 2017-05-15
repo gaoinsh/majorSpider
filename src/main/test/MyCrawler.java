43ea893f4fb0a90f9a99ee43e1e3f9773142e3ad
xiang.gao <121308941@qq.com>
2017-05-15 16:42:43 +0800

    crawlerDemo

---
+++ src/main/test/MyCrawler.java
@@ -0,0 +1,40 @@
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+import java.util.Set;
+import java.util.regex.Pattern;
+
+/**
+ * Created by xiang.gao on 2017/5/15.
+ * project majorSpider
+ */
+public class MyCrawler extends WebCrawler {
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(jpg|png))$");
+
+
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+        String link = url.getURL();
+        return FILTERS.matcher(link.toLowerCase()).matches();
+    }
+
+    @Override
+    public void visit(Page page) {
+        String url = page.getWebURL().getURL();
+        System.out.println("URL: " + url);
+
+        if (page.getParseData() instanceof HtmlParseData) {
+            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+            String text = htmlParseData.getText();
+            String html = htmlParseData.getHtml();
+            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+            System.out.println("Text length: " + text.length());
+            System.out.println("Html length: " + html.length());
+            System.out.println("Number of outgoing links: " + links.size());
+        }
+    }
+
+}
